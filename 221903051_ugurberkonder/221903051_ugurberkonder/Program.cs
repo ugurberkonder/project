d080e14d442867a1380d6705fa56dc4b1373d589
uğur berk önder <ugur.berkonder@outlook.com>
2020-10-30 20:46:33 +0300

    e

---
+++ 221903051_ugurberkonder/221903051_ugurberkonder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _221903051_ugurberkonder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
